fabfd3ee36b937cf247ce3dfb567feb941827432
FruGosky <lukaszkomandzik@gmail.com>
2023-06-29 22:55:22 +0200

    readme_&_footer_creators

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
 	return (
 		<div className="footer d-flex justify-content-center align-items-center bg-light">
-			<span>Strona stworzona przez nas</span>
+			<span>
+				Strona stworzona przez FruGosky, {'{2}'}, {'{3}'}.
+			</span>
 		</div>
 	);
 }
