2ecddee3d24ba6b50c59218f0e3b6bbb866a3c53
FruGosky <lukaszkomandzik@gmail.com>
2023-06-30 18:33:22 +0200

    slice_UserIconMenu_to_other_component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,9 @@
 import Menu from './Menu/Menu';
 import Logo from '../Logo/Logo';
-import useAuth from '../../hooks/useAuth';
-import userIcon from '../../assets/iconmonstr-user-20.svg';
-import styles from './Header.module.scss';
-import {
-	LINK as PROFILE_LINK,
-	TITLE as PROFILE_TITLE,
-} from '../../pages/Profile/Profile';
-import { Link } from 'react-router-dom';
+import UserIcon from './UserIconMenu/UserIconMenu';
 // import Searchbar from './Searchbar/Searchbar';
 
 export default function Header() {
-	const [auth, setAuth] = useAuth();
-
 	return (
 		<div>
 			<nav className="navbar bg-body-tertiary fixed-top">
@@ -25,55 +16,7 @@ export default function Header() {
 
 					{/* RIGHT SIDE OF HEADER */}
 					<div>
-						{!auth ? (
-							<button
-								className="btn btn-primary"
-								onClick={() => setAuth(true)}
-							>
-								Zaloguj się
-							</button>
-						) : (
-							<div className="d-flex align-items-center gap-3">
-								<div className="dropdown">
-									<a
-										className="dropdown-toggle"
-										href="/#"
-										role="button"
-										data-bs-toggle="dropdown"
-										aria-expanded="false"
-									>
-										<img
-											src={userIcon}
-											alt="User icon"
-											className={styles.userIcon}
-										/>
-									</a>
-									<ul
-										className={`dropdown-menu ${styles.dropdownMenu}`}
-									>
-										<li>
-											<Link
-												className="dropdown-item"
-												to={PROFILE_LINK}
-											>
-												{PROFILE_TITLE}
-											</Link>
-										</li>
-										<li>
-											<hr className="dropdown-divider" />
-										</li>
-										<li>
-											<button
-												className="dropdown-item"
-												onClick={() => setAuth(false)}
-											>
-												Wyloguj się
-											</button>
-										</li>
-									</ul>
-								</div>
-							</div>
-						)}
+						<UserIcon />
 					</div>
 				</div>
 			</nav>
---
+++ src/components/Header/UserIconMenu/UserIconMenu.tsx
@@ -0,0 +1,54 @@
+import userIcon from '../../../assets/iconmonstr-user-20.svg';
+import styles from './UserIconMenu.module.scss';
+import {
+	LINK as PROFILE_LINK,
+	TITLE as PROFILE_TITLE,
+} from '../../../pages/Profile/Profile';
+import { Link } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
+
+export default function UserIcon() {
+	const [auth, setAuth] = useAuth();
+
+	return !auth ? (
+		<button className="btn btn-primary" onClick={() => setAuth(true)}>
+			Zaloguj się
+		</button>
+	) : (
+		<div className="d-flex align-items-center gap-3">
+			<div className="dropdown">
+				<a
+					className="dropdown-toggle"
+					href="/#"
+					role="button"
+					data-bs-toggle="dropdown"
+					aria-expanded="false"
+				>
+					<img
+						src={userIcon}
+						alt="User icon"
+						className={styles.userIcon}
+					/>
+				</a>
+				<ul className={`dropdown-menu ${styles.dropdownMenu}`}>
+					<li>
+						<Link className="dropdown-item" to={PROFILE_LINK}>
+							{PROFILE_TITLE}
+						</Link>
+					</li>
+					<li>
+						<hr className="dropdown-divider" />
+					</li>
+					<li>
+						<button
+							className="dropdown-item"
+							onClick={() => setAuth(false)}
+						>
+							Wyloguj się
+						</button>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
