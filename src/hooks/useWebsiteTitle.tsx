620b53c02d070be06ed037dd56b95137f8d60d2b
FruGosky <lukaszkomandzik@gmail.com>
2023-06-30 14:37:01 +0200

    title_hoc_fix

--- src/hooks/useWebsiteTitle.tsx
+++ src/hooks/useWebsiteTitle.tsx
@@ -1,11 +1,16 @@
 import { useEffect } from 'react';
 
-export default function useWebsiteTitle(title: string) {
+export default function useWebsiteTitle(title?: string) {
 	const setTitle = (newTitle: string) => {
 		document.title = newTitle;
 	};
 
 	useEffect(() => {
+		if (!title) {
+			setTitle('DoIt-Healthy');
+			return;
+		}
+
 		setTitle(title);
 	}, [title]);
 
