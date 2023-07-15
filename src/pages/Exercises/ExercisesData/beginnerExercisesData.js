87e4571ee673212c278e54c4ddd36a31002f6a32
f384 <73945379+f384@users.noreply.github.com>
2023-07-15 15:53:32 +0200

    pages for different levels of exercises

--- src/pages/Exercises/ExercisesData/beginnerExercisesData.js
+++ src/pages/Exercises/ExercisesData/beginnerExercisesData.js
@@ -1,25 +1,34 @@
-//  "bench-press": {
-//             "title": "Bench press",
-//             "description": "...",
-//             "link": "http://youtu.be/bbGuHx07EDc"
-//           },
-//           "rowing": {
-//             "title": "Rowing",
-//             "description": "...",
-//             "link": "http://youtu.be/Nxr4-9ZXRBw"
-//           },
-//           "side-raises": {
-//             "title": "Side raises",
-//             "description": "",
-//             "link": "http://youtu.be/wzbX9cDlGDs"
-//           }
-
-const arr = ['bench-press', 'rowing', 'side-raises'];
-arr.map((ex, index) => {
-	return (
-		<div id={index}>
-			<span>{t(`${ex}.title`)}</span>
-			<div>{t(`${ex}.description`)}</div>
-		</div>
-	);
-});
+const arr = [
+	'squats',
+	'bench-press',
+	'rowing',
+	'side-raises',
+	'cable-crunch',
+	'calves',
+	'classic-deadlift',
+	'military-press',
+	'close-grip-chin-ups',
+	'barbell-reverse-lunge',
+	'abductors',
+	'plank',
+	'narrow-bench-press',
+];
+<div className="d-flex align-items-center justify-content-center">
+	{arr.map((ex) => {
+		return (
+			<div className="container">
+				<span className="header">
+					{translation(`page.exercises.beginners-plan.${ex}.title`)}
+				</span>
+				<div className="description">
+					{translation(
+						`page.exercises.beginners-plan.${ex}.description`
+					)}
+				</div>
+				<div className="link">
+					{translation(`page.exercises.beginners-plan.${ex}.link`)}
+				</div>
+			</div>
+		);
+	})}
+</div>;
