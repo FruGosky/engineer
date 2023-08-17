91e395f3fc0ca8f8bc8a4098384c715bee313dd1
FruGosky <lukaszkomandzik@gmail.com>
2023-08-17 15:43:09 +0200

    f_bmi-squash_merge

--- src/pages/Bmi/Bmi.tsx
+++ src/pages/Bmi/Bmi.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import LoadingIcon from '../../components/LoadingIcon/LoadingIcon';
 import useWebsiteTitle from '../../hooks/useWebsiteTitle';
 import { useTranslation } from 'react-i18next';
-
+import BmiCalculator from './BmiComponents/BmiCalculator';
 export const BMI_TITLE = 'page.bmi.title';
 export const BMI_LINK = '/bmi';
 
@@ -11,8 +11,6 @@ export default function Bmi() {
 	const { t: translation } = useTranslation();
 
 	const TRANSLATED_TITLE = translation(BMI_TITLE);
-	// TODO! DELETE THIS TRANSLATION BELOW WHEN U WILL START ADDING SOME CONTENT TO THIS PAGE AND ADD YOURS TRANSLATION DO NOT FORGET TO ADD EN AND PL TRANSLATION NOT ONLY ONE ITS VERY VERY IMPORTANT TO HAVE ALL TRANSLATIONS
-	const THIS_IS_PAGE = translation('to-be-deleted.this-is-page');
 
 	useWebsiteTitle(TRANSLATED_TITLE);
 
@@ -26,8 +24,8 @@ export default function Bmi() {
 	return loading ? (
 		<LoadingIcon />
 	) : (
-		<div className="d-flex align-items-center justify-content-center">
-			{`${THIS_IS_PAGE} ${TRANSLATED_TITLE}`}
+		<div className="d-flex align-items-center justify-content-center ">
+			<BmiCalculator />
 		</div>
 	);
 }
