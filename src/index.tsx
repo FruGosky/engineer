b6c4f2dbca954ef72d496dc8e3993d113ace7c40
FruGosky <lukaszkomandzik@gmail.com>
2023-12-21 17:40:56 +0100

    web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import './index.scss';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import { getLCP, getFID, getCLS } from 'web-vitals';
 
 // For React v18
 const root = ReactDOM.createRoot(
@@ -17,6 +18,25 @@ root.render(
 		<App />
 	</React.StrictMode>
 );
+
+getLCP((metric) => {
+	if (metric.value > 3000) {
+		console.log('LCP', metric);
+	}
+}, true);
+
+getFID((metric) => {
+	if (metric.value > 100) {
+		console.log('FID', metric);
+	}
+}, true);
+
+getCLS((metric) => {
+	if (metric.value > 0.25) {
+		console.log('CLS', metric);
+	}
+}, true);
+
 // For React v17
 // ReactDOM.render(
 // 	<React.StrictMode>
